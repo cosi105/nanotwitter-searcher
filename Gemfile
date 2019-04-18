27c3a6dc59dad59ba2429a6b6351f5b6485725a5
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-18 04:05:58 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'bunny'
+gem 'minitest'
+gem 'rack'
+gem 'rack-test'
+gem 'rake'
+gem 'rb-readline'
+gem 'redis'
+gem 'sinatra'
+gem 'simplecov'
+gem 'puma'
+
+
+group :development do
+  gem 'pry-byebug'
+end
---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'bundler'
+require 'json'
+Bundler.require
+require 'pry-byebug'
+
+set :port, 8080 unless Sinatra::Base.production?
+
+# Initialize RabbitMQ object & connection appropriate to
+# the current env (viz., local vs. prod)
+if Sinatra::Base.production?
+  rabbit = Bunny.new(ENV['CLOUDAMQP_URL'])
+else
+  rabbit = Bunny.new(automatically_recover: false)
+end
+rabbit.start
+channel = rabbit.create_channel
+RABBIT_EXCHANGE = channel.default_exchange
+
+# author_id, tweet_id, tweet_body
+NEW_TWEET = channel.queue('new_tweet.tweet_data')
+
+NEW_TWEET.subscribe(block: false) do |delivery_info, properties, body|
+  parse_tweet_tokens(JSON.parse(body)['tweet_body'])
+end
+
+def parse_tweet_tokens(tweet_body)
+  puts tweet_body.split
+end
