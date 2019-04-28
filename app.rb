8786e5dfeb00c102f1e44e912a74538a14cf0458
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-28 13:54:51 -0400

    Adds seed handling

--- app.rb
+++ app.rb
@@ -19,10 +19,16 @@ end
 rabbit.start
 channel = rabbit.create_channel
 RABBIT_EXCHANGE = channel.default_exchange
-
 # author_id, tweet_id, tweet_body
 NEW_TWEET = channel.queue('new_tweet.tweet_data')
+seed = channel.queue('searcher.seed')
 
+# Parses & indexes tokens from payload.
+seed.subscribe(block: false) do |delivery_info, properties, body|
+  seed_from_payload(JSON.parse(body))
+end
+
+# Extracts Tweet body from payload & indexes its tokens.
 NEW_TWEET.subscribe(block: false) do |delivery_info, properties, body|
   parse_tweet_tokens(JSON.parse(body))
 end
@@ -32,5 +38,12 @@ def parse_tweet_tokens(tweet)
   tokens = tweet['tweet_body'].split.map(&:downcase)
   tokens.each do |token|
     REDIS.lpush(token, tweet_id)
+  end
+end
+
+# Parses & indexes tokens from each Tweet body in payload.
+def seed_from_payload(payload)
+  payload.each do |tweet_body|
+    parse_tweet_tokens(tweet_body)
   end
 end
